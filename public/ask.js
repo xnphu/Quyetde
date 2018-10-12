881abfc413f0b62f6846e19aeb3ab2b79d18520a
Xnphu <immrp188@gmail.com>
2018-10-12 22:52:48 +0700

    random other question using Jquery

--- public/ask.js
+++ public/ask.js
@@ -1,11 +1,18 @@
-const maxLength = 200;
-const questionContentElem = document.getElementById('questionContent');
-const remainCharElem = document.getElementById('remain');
+// const maxLength = 200;
+// const questionContentElem = document.getElementById('questionContent');
+// const remainCharElem = document.getElementById('remain');
 
-questionContentElem.addEventListener(
-    'input', 
-    function() {
-        var remainChar = maxLength - questionContentElem.value.length;
-        remainCharElem.innerText = remainChar;
-    }
-);
+// questionContentElem.addEventListener(
+//     'input', 
+//     function() {
+//         var remainChar = maxLength - questionContentElem.value.length;
+//         remainCharElem.innerText = remainChar;
+//     }
+// );
+
+const maxLength = 200;
+
+$("#questionContent").on('input', function() {
+            var remainChar = maxLength - $("questionContent").val().length;
+            $("remainChar").text(remainChar);      
+});
--- public/main.js
+++ public/main.js
@@ -1,9 +1,31 @@
-axios.get("http://localhost:1808/randomquestion")
-.then(function (response) {
-    if(response.data) {
-        document.getElementById("questionContent").innerText = response.data.questionContent;
-    }
-})
-.catch(function (error) {
-	console.log(error);
-}); 
+// axios.get("http://localhost:1808/randomquestion")
+// .then(function (response) {
+//     if(response.data) {
+//         document.getElementById("questionContent").innerText = response.data.questionContent;
+//     }
+// })
+// .catch(function (error) {
+// 	console.log(error);
+// }); 
+
+function getRandomQuestion() {
+	$.ajax({
+		url: "http://localhost:1808/randomquestion",
+		type: "GET",
+		success: function(response) {
+			if(response) {
+				$("#questionContent").text(response.questionContent);
+				$(".answer_btn").data("questionid", response.id);
+			}
+		},
+		error: function(err) {
+			console.log(err);
+		}
+	});
+}
+
+getRandomQuestion();
+
+$("#otherQuestion").on("click", function() {
+	getRandomQuestion();
+});
